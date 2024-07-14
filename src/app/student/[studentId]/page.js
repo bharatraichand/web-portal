2a0d1df515bcc0f096505d51875164cb8d299f27
jinish08 <jinishshah08@gmail.com>
2024-07-14 18:09:30 +0530

    minor change

--- src/app/student/[studentId]/page.js
+++ src/app/student/[studentId]/page.js
@@ -15,7 +15,7 @@ export default function Page ({params}) {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <StudentProfile studentId={studentId} />
-    </div>
+    </div> 
   )
 }
 
